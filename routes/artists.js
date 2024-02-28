3820dee301d4af8897fe9dcdb3295e13be93741f
Jens Andreasson <jens.andreasson@ntig.se>
2024-02-28 11:04:47 +0100

    artists routes

---
+++ routes/artists.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+const pool = require('../db')
+
+// get /artists
+router.get('/', async (req, res) => {
+
+  const [artists] = await pool.promise().query('SELECT * FROM jens_artist')
+
+  console.log(artists)
+
+  res.render('artists.njk', {
+    title: 'All artists',
+    artists
+  })
+})
+
+// get /artists/:id
+router.get('/:id', (req, res) => {
+  res.send("artist")
+})
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const artistsRouter = require('./routes/artists')
+
 const app = express()
 
 nunjucks.configure('views', {
@@ -15,6 +17,8 @@ app.get('/', (req, res) => {
     title: "Landing page"
   })
 })
+
+app.use('/artists', artistsRouter)
 
 app.listen(3000, () => {
   console.log('Server is listening on http://localhost:3000')
