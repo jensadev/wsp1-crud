122259b425620104093c90e5b2686554eadb36dc
Jens Andreasson <jens.andreasson@ntig.se>
2024-02-29 12:59:32 +0100

    validation

--- routes/artists.js
+++ routes/artists.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { body, matchedData, validationResult } = require('express-validator')
 
 const pool = require('../db')
 
@@ -9,43 +10,53 @@ router.get('/', async (req, res) => {
 
   res.render('artists.njk', {
     title: 'All artists',
-    artists
+    artists,
   })
 })
 
 router.get('/new', (req, res) => {
   res.render('newartist.njk', {
-    title: 'Create new artist'
+    title: 'Create new artist',
   })
 })
 
-router.post('/new', async (req, res) => {
-  console.log(req.body.name)
-  // INSERT INTO `jens_artist` (`name`) VALUES ('Dream Theater');
-  const name = req.body.name // tvätta och validera
+router.post(
+  '/new',
+  body('name').notEmpty().trim().escape(),
+  async (req, res) => {
 
-  const [result] = await pool.promise().query('INSERT INTO jens_artist (name) VALUES (?)', [name])
+    const result = validationResult(req);
 
-  if (result.affectedRows === 1) {
-    res.send('form posted, artist created')
-  } else {
-    console.log(result)
-    // meddela att något gick fel
-    res.redirect('/artists/new')
+    if (result.isEmpty()) {
+      const data = matchedData(req);
+      const [dbResult] = await pool
+        .promise()
+        .query('INSERT INTO jens_artist (name) VALUES (?)', [data.name])
+
+      if (dbResult.affectedRows === 1) {
+        res.send('form posted, artist created')
+      } else {
+        // server error
+        res.status(500)
+      }
+    } else {
+      res.send({ errors: result.array() });
+    }
   }
-
-})
+)
 
 // get /artists/:id
 router.get('/:id', async (req, res) => {
   const id = parseInt(req.params.id)
   if (Number.isInteger(id)) {
     // använd id hämta från databas
-    const [artist] = await pool.promise().query('SELECT * FROM jens_artist WHERE id = ?', [id])
+    const [artist] = await pool
+      .promise()
+      .query('SELECT * FROM jens_artist WHERE id = ?', [id])
 
     res.render('artist.njk', {
       title: artist[0].name,
-      artist: artist[0]
+      artist: artist[0],
     })
   } else {
     // meddela användaren att något gick fel
@@ -53,7 +64,5 @@ router.get('/:id', async (req, res) => {
     res.redirect('/artists')
   }
 })
-
-
 
 module.exports = router
